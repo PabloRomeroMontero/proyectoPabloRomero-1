c6745897844f2feb367962265d42359d21829703
Ricardo Pedrosa <rickpedrosa94@gmail.com>
2019-12-06 20:52:33 +0100

    layouts login y navbar hechos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {DetailComponent} from './components/detail/detail.component';
+import {BuscarComponent} from './components/buscar/buscar.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegistrarComponent} from './components/registrar/registrar.component';
 
-const routes: Routes = [];
+let routes: Routes;
+routes = [
+  {  component: HomeComponent, path: 'home', canActivate: []},
+  {  component: DetailComponent, path: 'detail/:id', canActivate: []},
+  {  component: BuscarComponent, path: 'buscar/:id'},
+  {  component: LoginComponent, path: 'login'},
+  {  component: RegistrarComponent, path: 'registrar'},
+  {  component: HomeComponent, path: '**', canActivate: []},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './components/login/login.component';
+import {HomeComponent} from './components/home/home.component';
+import {RegistrarComponent} from './components/registrar/registrar.component';
+import {DetailComponent} from './components/detail/detail.component';
+import {ItemNewComponent} from './components/item-new/item-new.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {BuscarComponent} from './components/buscar/buscar.component';
+import {CreateEditComponent} from './components/create-edit/create-edit.component';
+import {NewsService} from './services/news.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegistrarComponent,
+    DetailComponent,
+    ItemNewComponent,
+    NavbarComponent,
+    BuscarComponent,
+    CreateEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [NewsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
